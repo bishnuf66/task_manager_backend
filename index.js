457c37baa5c3d18aae54cf2b44cf2a2e8532a7a7
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-08 20:41:37 +0545

    updated cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: "*",
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
