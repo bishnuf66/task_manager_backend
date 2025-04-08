15da4659c232720bd5ad00f40d0680ba6f221fe2
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-08 18:27:57 +0545

    new migration file

--- prisma/migrations/20250408124245_y/migration.sql
+++ prisma/migrations/20250408124245_y/migration.sql
@@ -1,20 +1,18 @@
 -- CreateTable
-CREATE TABLE `SubTask` (
+CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NULL,
-    `isCompleted` BOOLEAN NOT NULL DEFAULT false,
-    `taskId` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Task` (
     `id` VARCHAR(191) NOT NULL,
-    `status` ENUM('PENDING', 'IN_PROGRESS', 'COMPLETED', 'CANCELED') NOT NULL DEFAULT 'PENDING',
     `priority` ENUM('LOW', 'MEDIUM', 'HIGH', 'URGENT') NOT NULL DEFAULT 'MEDIUM',
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
@@ -23,9 +21,10 @@ CREATE TABLE `Task` (
     `isCompleted` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `SubTask` ADD CONSTRAINT `SubTask_taskId_fkey` FOREIGN KEY (`taskId`) REFERENCES `Task`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Task` ADD CONSTRAINT `Task_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
