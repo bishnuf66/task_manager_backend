880de7edbf71059d08f73d4efe0f17d4c2edbcc1
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-08 21:02:10 +0545

    removed cors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require("cors");
 const app = express();
 require('dotenv').config();
 
@@ -9,24 +8,6 @@ const taskRoutes = require('./routes/TaskRoutes');
 // Middleware
 app.use(express.json());
 
-// 🔧 CORS setup
-const allowedOrigins = [
-    process.env.FRONTEND_URL,
- 
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin); // allow request
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // allow cookies & auth headers
-}));
 
 // Routes
 app.use(authRoutes);
