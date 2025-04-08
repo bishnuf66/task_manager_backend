7220876f049bac0c61cd432384620080d2123653
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-08 21:10:02 +0545

    cors error

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require("cors");
 
 const authRoutes = require('./routes/AuthRoute');
 const taskRoutes = require('./routes/TaskRoutes');
 
 // Middleware
 app.use(express.json());
-
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,PUT,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  };
+  app.use(cors(corsOptions));
 
 // Routes
 app.use(authRoutes);
