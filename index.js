aaa053c30db6df2f42a5b51e33113fbd1e92e479
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-08 20:45:34 +0545

    latest code

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const taskRoutes = require('./routes/TaskRoutes')
 
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "*",
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: "*",
+    
+}));
 
 const port =process.env.PORT || 8000;
 app.use([authRoutes])
